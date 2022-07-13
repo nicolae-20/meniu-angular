ad847460c45f68f0dbb66be3c038cf0f0038f584
Nicolae <nicupanait88@gmail.com>
2022-07-13 13:19:25 +0300

    schimbari pagina login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -12,11 +13,11 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'recipies', component: NavbarComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'auth', component: AuthComponent },
   { path: 'products', component: ProduseComponent },
   { path: 'navbar', component: NavbarComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', pathMatch: 'full', 
-  component: PageNotFoundComponent },
+  { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
 ];
 
 @NgModule({
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-auth',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AuthComponent {
 
-}
+    isLogged: boolean = true;
+
+    minLengthPass: number = 15;
+  
+    email = new FormControl('', [Validators.required, Validators.email]);
+    password = new FormControl('', [Validators.required, Validators.minLength(this.minLengthPass)])
+    hide = true;
+  
+    onSwitchMode() {
+        this.isLogged = !this.isLogged;
+    }
+
+    getErrorMessage() {
+      if (this.email.hasError('required')) {
+        return 'Trebuie sa introduci un email';
+      }
+  
+      return this.email.hasError('email') ? 'Email invalid' : '';
+    }
+  
+    getPasswordMessage() {
+      if (this.password.validator?.length)
+      {
+        return 'Parola trebuie sa contina minimum 15 caractere';
+      }
+
+        return;
+    }
+  }
+  
